c8fbc44b0416681d59752ec220bb01a300ac8030
Abdulloh Umar <107607543+dev-au@users.noreply.github.com>
2025-01-23 14:08:02 +0500

    Update start.py

--- bot_src/handlers/start.py
+++ bot_src/handlers/start.py
@@ -13,7 +13,9 @@ users = {}
 async def start_command(message: types.Message):
     keyboard = types.InlineKeyboardButton(text='Kino qidirsh', switch_inline_query_current_chat='')
     reply_markup = types.InlineKeyboardMarkup(inline_keyboard=[[keyboard]])
-    await message.reply('Kino botga xush kelibsiz', reply_markup=reply_markup)
+    await message.reply('Kino botga xush kelibsiz, kino qidirish uchun quyidagi tugmani bosing. '
+                    'Televizorda tomosha qilish uchun esa Televizordan https://4fun.uz/ saytiga kiring va botga kodni /tv <kod> shaklida kiriting.\n'
+                    'Masalan /tv 1403', reply_markup=reply_markup)
 
 
 @dp.message(F.text.startswith('/tv'))
